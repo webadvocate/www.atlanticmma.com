7c01e9df4ab774a027ca3e8588d2487cf2732217
Michael Klett <moklett@gmail.com>
2020-04-18 15:53:22 -0400

    Mimicking dynamic site URLs from static site
    This converts all pages/URLs like the following:
    
    ```
    /zip/zap.html
    ```
    
    to:
    
    ```
    /zip/zap/index.html
    ```
    
    This way, old URLs that looked like `/zip/zap` will render the file
    `zip/zap/index.html` under the URL `/zip/zap/`.
    
    All hrefs were updated programmatically.

--- script/correct_urls.rb
+++ script/correct_urls.rb
@@ -21,7 +21,7 @@ def convert_files!
     directory_to_create = File.join(
       File.dirname(original_filename),
       File.basename(original_filename, ".html")
-    )
+    ).gsub("./", "")
 
     if !Dir.exist?(directory_to_create)
       puts "Creating directory #{directory_to_create}"
@@ -38,7 +38,7 @@ def convert_files!
     end
 
     old_href = %{href="#{original_filename}"}
-    new_href = %{href="/#{directory_to_create}/"}.gsub("//", "/")
+    new_href = %{href="/#{directory_to_create}/"}
     each_html_file do |fn|
       original_contents = File.read(fn)
       updated_contents = original_contents.gsub(old_href, new_href)
@@ -66,6 +66,7 @@ def convert_refs!
     updated_contents = updated_contents.gsub(%{"images/}, %{"/images/})
     updated_contents = updated_contents.gsub(%{"index.html}, %{"/index.html})
     updated_contents = updated_contents.gsub(%{<script src='http://cdn.goroost.com/roostjs/7bc6889c1b38471c93a34a7a5dbd7e64' async></script>}, "")
+    updated_contents = updated_contents.gsub(%{<script src='//cdn.goroost.com/roostjs/7bc6889c1b38471c93a34a7a5dbd7e64' async></script>}, "")
     if updated_contents != original_contents
       File.write(filename, updated_contents)
     end
